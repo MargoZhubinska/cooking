94436a59399e952f4dd7315e5b43aff9d7742f1e
MargoZhubinska <165931733+MargoZhubinska@users.noreply.github.com>
2024-06-14 18:23:51 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,27 @@
-Develop a simple book management application with ArrayList.
-User should be able to add a book to ArrayList.
-User should be able to remove a book from ArrayList.
+Develop a program that contains multiple classes.
 
-Easy: Work with String in ArrayList. All the actions should be available for user.
+Choose one of the topics.
 
-Medium: Work with String User should be able to repeat all the actions infinitely.
+Easy: Cheese shop
 
-Hard: Create a Book class and work with Book object to the ArrayList.
+Should contain these classes.
 
-  python
-  Develop a simple prototype for an online shop
-1. Define classes Client, Items, Transaction
-2. Write the logic so that each client can purchase one of several items in a single transaction
-3. Make a printout of data with nested loops
-4. The program should print out:
-- all clients
-- all transactions of a client
-- all items purchased in each transaction of the client
+Cheese 
+
+CheeseShop - gives access for the shop owner to add/remove different cheeses, gives access for the customer to buy different cheeses, remove cheese from cart, should be possible to get all the cheeses from the cart or available cheeses in the store
+
+Main - User UI, accesses CheeseService and CheeseShop to buy or to add different cheeses.
+
+Should be possible to use 12.06.2024 lesson as an example and/or check out at discord the teamwork from other students.
+
+
+
+Medium: same as easy, but add 2 more classes
+
+CheeseService - gives access for the shop owner to add different cheeses (should remove this functionality from CheeseShop)
+
+Customer - contains money the customer has, contains the items the customer owns. Whenever the customer buys something, money is reduced. If customer doesn't have any money left, then notify the user about it.
+
+
+
+Hard: Same as medium, but save all the data in JSON.txt file. Should be able to save all the data in JSON and when opening the program again, it should save all the information.
