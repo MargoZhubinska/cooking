20e8194bb4a5e8af73a636e921395e990bad6466
MargoZhubinska <165931733+MargoZhubinska@users.noreply.github.com>
2024-06-14 18:25:15 +0300

    Update main.java

--- main.java
+++ main.java
@@ -25,3 +25,15 @@ Customer - contains money the customer has, contains the items the customer owns
 
 
 Hard: Same as medium, but save all the data in JSON.txt file. Should be able to save all the data in JSON and when opening the program again, it should save all the information.
+
+Python
+Easy Level
+In this level, you will read the data from prices.txt (see additional files),  compute the total price of all items, and count the total number of purchased items. 
+
+! If you don't know how to deal with non integer characters, remove them manually from the file !
+
+Medium Level
+In this level, we will extend the program to skip rows that contain anything other than a number in the code. (Modify your program in such a way that all rows that contain something else than a number, are skipped.)
+
+
+  
